043a100066f9cf25eb86c859cb899f2b3dd5f95c
real-hassnain <hassnainp2@gmail.com>
2026-04-13 23:33:07 +0500

    Add files via upload

---
+++ list_task.py
@@ -0,0 +1,13 @@
+nums=[5, 10, 15, 20, 25]
+print("First Element: ",nums[0])
+print("Last Element: ",nums[-1])
+
+print("All Elements: ")
+for i in nums:
+	print(i)
+
+print("Numbers greater than 10: ")
+for i in nums:
+	if i>10:
+		print(i)
+
